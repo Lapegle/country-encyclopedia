ce3f3e5ba0cbb54caff213ce4a5a8941f0be3eed
Daniels Gabranovs <daniels.gabranovs@midis.eu>
2024-02-03 14:30:59 +0200

    Styling adjustments in country detail page

--- resources/js/Pages/Countries/Detail.vue
+++ resources/js/Pages/Countries/Detail.vue
@@ -16,18 +16,18 @@ defineProps({country: Object})
                     <div>
                         <h1 class="font-bold text-4xl text-center">{{ country.common_name }}</h1>
                         <div class="flex flex-col sm:flex-row-reverse mt-4">
-                            <div class="sm:ml-8">
+                            <div class="sm:ml-52 flex justify-center">
                                 <img
                                     :alt="'Flag of ' + country.official_name"
                                     :src="country.flag_url"
                                     class="w-48"
                                 />
                             </div>
-                            <div>
+                            <div class="mt-4 sm:mt-0">
                                 <TextInfoItem :value="country.official_name" description="Official name"/>
                                 <TextInfoItem :value="country.country_code.toUpperCase()" description="Country code"/>
                                 <TextInfoItem :value="country.population" description="Population"/>
-                                <TextInfoItem :value="country.area" description="Area"/>
+                                <TextInfoItem :value="country.area" description="Area (km²)"/>
                             </div>
                         </div>
                     </div>
