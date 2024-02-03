9be7d6966114073226f1eb6518f16ec94bcec088
Daniels Gabranovs <daniels.gabranovs@midis.eu>
2024-02-03 13:25:41 +0200

    Add debounce to search

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -1,47 +1,57 @@
 <script setup>
+import {Head} from '@inertiajs/vue3';
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
-import { Head } from '@inertiajs/vue3';
-import { ref, watch } from "vue";
 import TextInput from "@/Components/TextInput.vue";
 import InputLabel from "@/Components/InputLabel.vue";
+import {ref, watch} from "vue";
+import {debounce} from "lodash";
 
 const search = ref('')
 const searchFocused = ref(false)
 const countries = ref([])
 
-watch(search, async (newSearch, oldSearch) => {
+const getCountries = debounce((search) => {
     axios.get('/findCountry', {
         params: {
-            search: newSearch
+            search
         }
-    }).then((response) => {
-        countries.value = response.data
     })
+        .then((response) => {
+            countries.value = response.data;
+        })
+        .catch((error) => {
+            console.error('Error fetching countries:', error);
+        });
+}, 300);
+
+watch(search, async (newSearch, oldSearch) => {
+    if (newSearch.length) {
+        getCountries(newSearch)
+    } else {
+        countries.value = []
+    }
 })
 </script>
 
 <template>
-    <Head title="Home" />
+    <Head title="Home"/>
 
     <AuthenticatedLayout>
         <div class="py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div class="bg-white shadow-sm sm:rounded-lg flex justify-center p-8">
                     <div class="w-full relative">
-                        <InputLabel for="search" value="Search for a country" />
+                        <InputLabel for="search" value="Search for a country"/>
                         <TextInput
-                            class="w-full"
                             id="search"
-                            name="search"
                             v-model="search"
-                            @focus="searchFocused = true"
+                            class="w-full"
+                            name="search"
                             @blur="searchFocused = false"
+                            @focus="searchFocused = true"
                         />
                         <div v-if="searchFocused" class="absolute z-10 w-full mt-1 bg-white rounded-md shadow-lg">
-                            <ul
-                                tabindex="-1"
-                                class="py-1 overflow-auto text-base leading-6 rounded-md shadow-xs max-h-60 focus:outline-none sm:text-sm sm:leading-5"
-                            >
+                            <ul class="py-1 overflow-auto text-base leading-6 rounded-md shadow-xs max-h-60 focus:outline-none sm:text-sm sm:leading-5">
                                 <div v-for="country in countries" :key="country.country_code">
                                     <li
                                         class="relative py-2 pl-3 text-gray-900 cursor-pointer hover:bg-indigo-200"
