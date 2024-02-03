dc54ef42ca2053eee6d5e59b0fb105edbc5021db
Daniels Gabranovs <daniels.gabranovs@midis.eu>
2024-02-03 15:40:08 +0200

    Add language detail page

---
+++ resources/js/Components/CountryCard.vue
@@ -0,0 +1,19 @@
+<script setup>
+import {Link} from "@inertiajs/vue3";
+
+defineProps({country: Object})
+</script>
+
+<template>
+    <Link
+        :href="'/countries/' + country.id"
+        class="flex w-full px-2 bg-gray-100 hover:bg-gray-200 shadow m-2 py-2 rounded max-h-32 items-center"
+    >
+        <img
+            :alt="'Flag of ' + country.official_name"
+            :src="country.flag_url"
+            class="w-28"
+        />
+        <p class="ml-4 font-bold">{{ country.common_name }}</p>
+    </Link>
+</template>
--- resources/js/Pages/Languages/Detail.vue
+++ resources/js/Pages/Languages/Detail.vue
@@ -1,6 +1,7 @@
 <script setup>
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout.vue";
 import {Head} from "@inertiajs/vue3";
+import CountryCard from "@/Components/CountryCard.vue";
 
 defineProps({language: Object})
 </script>
@@ -11,8 +12,9 @@ defineProps({language: Object})
     <AuthenticatedLayout>
         <div class="py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                <div class="bg-white shadow-sm sm:rounded-lg flex justify-center p-8">
-                    <h1 class="font-bold text-4xl text-center">{{ language.name }}</h1>
+                <div class="bg-white shadow-sm sm:rounded-lg flex flex-col justify-center p-8">
+                    <h1 class="font-bold text-4xl text-center">Countries where {{ language.name }} is spoken</h1>
+                    <CountryCard v-for="country in language.countries" :country="country"/>
                 </div>
             </div>
         </div>
